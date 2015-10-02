47420d9ea0489dda6e6e94d9921e2581e0d4a811
soniahachemi <so.hachemi@gmail.com>
2015-10-02 19:32:51 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,16 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		f = new Fenetre();
-		f.changerFond(new Menu());
+		Decor decor = new Decor();
+		f.changerFond(decor);
+
+		
+		Oiseau o = new Oiseau();
+		o.placer(decor.getPlan());
+		decor.add(o);
+		
+		Runnable a = new Animation(o,decor,1,30.0);
+		new Thread(a).start();
 	}
 
 	public static Fenetre getFenetre(){
