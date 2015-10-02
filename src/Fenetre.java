a3660faa9cf11ffe98dcd011ae540900e3124154
soniahachemi <so.hachemi@gmail.com>
2015-10-02 21:18:50 +0200

    Update Fenetre.java

--- src/Fenetre.java
+++ src/Fenetre.java
@@ -1,8 +1,17 @@
+import javax.swing.JFrame;
+
 
 public class Fenetre extends JFrame {
 
 	public Fenetre(){
 		this.setSize(Constante.largeur,Constante.hauteur);
 		this.setVisible(true);
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+
+	public void changerFond(Decor decor) {
+		// TODO Auto-generated method stub
+		setContentPane(decor);
+		revalidate();
 	}
 }
