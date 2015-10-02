4df859c64498a737c15d852e5ea612aac56f7400
soniahachemi <so.hachemi@gmail.com>
2015-10-02 19:34:25 +0200

    Create Constante.java

---
+++ src/Constante.java
@@ -0,0 +1,19 @@
+
+public class Constante {
+
+	public static final int largeur = 1000;
+	public static final int hauteur = 700;
+	
+	// 1 m = 20px
+	public static final int echelle = 20;
+	
+
+	public static final int hauteurSol = 100;
+	// On place le "lance pierre" à x=50
+	public static final int debTrajX = 50;
+	// On place le "lance pierre" à 50 px au dessus du sol
+	public static final int hauteurLP = 50;
+
+
+	
+}
