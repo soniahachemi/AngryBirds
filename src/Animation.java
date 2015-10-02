72966cc799948fd0a3df2cc99e263e122b517c63
soniahachemi <so.hachemi@gmail.com>
2015-10-02 19:34:01 +0200

    Create Animation.java

---
+++ src/Animation.java
@@ -0,0 +1,33 @@
+
+public class Animation extends Thread{
+
+	private Oiseau o;
+	private Decor d;
+	private double vitesse,angle;
+	
+	public Animation(Oiseau o1, Decor d1, double vitesse1, double angle1){
+		o=o1;
+		d=d1;
+		vitesse=vitesse1;
+		angle=angle1;
+	}
+	
+	public void run(){
+		long t=0;
+		System.out.println(o.getCoord().getY());
+		while(t<100000){
+			try{
+				o.placer(d.getPlan());
+				Main.getFenetre().revalidate();
+				double rad = Math.toRadians(angle);
+				o.setX((int)((vitesse*Math.cos(rad))*t));
+				o.setY((int)((-(1/2)*9.81*Math.sqrt(t)+(vitesse*Math.sin(rad)*t))));
+				System.out.println(o.getCoord().getY());
+				sleep(10);
+				t+=10;
+			}catch(Exception e){
+				
+			}
+		}
+	}
+}
