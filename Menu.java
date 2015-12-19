2b00a16b7816dd6c8bb269d1dbd62c7b45f54692
quentinspinnewyn <quentin.spinnewyn@outlook.fr>
2015-12-19 14:57:44 +0100

    Update Menu.java
    non fini, fond beugué

--- Menu.java
+++ Menu.java
@@ -4,8 +4,7 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Image;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
@@ -14,9 +13,6 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-
-import Main.Fenetre;
-import Main.Main;
 
 
 /**
@@ -43,14 +39,14 @@ public class Menu extends JFrame{
 	 * Defini toute l interface presente dans la fenetre menu
 	 */
 	public Menu(int l, int h){
-		//f = new Fenetre(1024,576);
+
 		super();
 		JPanel p = new JPanel();
 		p.setLayout(null);
 		
 		this.setSize(l,h);
-		this.setResizable(false);
-		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		//this.setResizable(false);
+		//this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		
 		titre = new JLabel("Bienvenue sur Angry Birds !");
 		titre.setBounds(50,25,500,30);
@@ -65,7 +61,7 @@ public class Menu extends JFrame{
 		p.add(soustitre);
 		
 		jouer = new JButton("Jouer");
-		jouer.setBounds(390,200,200,60);
+		jouer.setBounds(110,180,200,60);
 		jouer.setFont(new Font("", Font.CENTER_BASELINE, 15));
 		jouer.setForeground(Color.RED);
 		/*
@@ -77,45 +73,46 @@ public class Menu extends JFrame{
 		p.add(jouer);
 		
 		regles = new JButton("Règles du jeu");
-		regles.setBounds(390,270,200,60);
+		regles.setBounds(110,250,200,60);
 		regles.setFont(new Font("", Font.CENTER_BASELINE, 15));
 		regles.setForeground(Color.RED);
 		p.add(regles);
 		
 		credits = new JButton("Crédits");
-		credits.setBounds(390,340,200,60);
+		credits.setBounds(110,320,200,60);
 		credits.setFont(new Font("",Font.CENTER_BASELINE, 15));
 		credits.setForeground(Color.RED);
+		/*
+		credits.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0){
+			    new Credits();
+			}
+		});
+		 */
+		 
 		p.add(credits);
 		
 		options = new JButton("Options");
-		options.setBounds(390,410,200,60);
+		options.setBounds(110,390,200,60);
 		options.setFont(new Font("",Font.CENTER_BASELINE, 15));
 		options.setForeground(Color.RED);
 		p.add(options);
-		
-
+			
 		this.add(p);
 		this.setVisible(true);
+			
+	}
+	
 
+	public void paintComponent(Graphics g){
+		try {
+			BufferedImage img = ImageIO.read(new File("ressources/fond.jpg"));	// (A)
+			g.drawImage(img, 0, 0, this.getWidth(), this.getHeight(), this); // Pour que la position et les dimensions de l'image concordent avec celles du fond
+		} catch (IOException e){}
 	}
 	
 	
-	
-	private void setContentPane(Image image2) {
-		// TODO Auto-generated method stub
-		
-	}
 
-
-/*
-		//Fond du menu
-		public void paintComponent(Graphics g) throws IOException {
-				img = ImageIO.read(new File("ressources"+File.separator+"fond.jpeg"));
-				g.drawImage(img, 0, 0, this);
-		}
-	*/
-	
 	
 	
 	
